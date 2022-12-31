258b87273ce90ebc8b978b00b42517056dfe30fb
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-12-31 17:56:26 +0900

    feat: 表示しているイベントをハイライト

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const EventList = ({ events }) => {
   const renderEvents = (eventArray) => {
+    const baseClassName = 'block border-b pt-2 px-1.5 pb-2.5'
+    const activeClassName = [baseClassName, 'bg-primary text-primary-content hover:bg-primary-focus'].join(' ');
+    const inactiveClassName = [baseClassName, 'hover:bg-primary hover:text-primary-content'].join(' ');
+
     eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
 
     return eventArray.map((event) => (
       <li key={event.id}>
-        <Link to={`/events/${event.id}`} className='block border-b pt-2 px-1.5 pb-2.5 hover:bg-primary hover:text-primary-content'>
+        <NavLink to={`/events/${event.id}`} className={({ isActive }) => isActive ? activeClassName : inactiveClassName}>
           {event.event_date}
           {' - '}
           {event.event_type}
-        </Link>
+        </NavLink>
       </li>
     ));
   };
