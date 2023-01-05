ef6ecde9aab665ce6cb202311c5f6c832f1d9357
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-01-05 10:28:26 +0900

    feat: 日付フィールドに pikaday を適用

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react"
-import { isEmptyObject, validateEvent } from "../helpers/helpers"
+import React, { useEffect, useRef, useState } from "react"
+import { formatDate, isEmptyObject, validateEvent } from "../helpers/helpers"
+import Pikaday from "pikaday"
 
 const EventForm = () => {
   const [event, setEvent] = useState({
@@ -13,12 +14,31 @@ const EventForm = () => {
 
   const [formErrors, setFormErrors] = useState({})
 
+  const dateInput = useRef(null)
+
+  useEffect(() => {
+    const p = new Pikaday({
+      field: dateInput.current,
+      onSelect: (date) => {
+        const formattedDate = formatDate(date)
+        dateInput.current.value = formattedDate
+        updateEvent("event_date", formattedDate)
+      },
+    })
+
+    return () => p.destroy()
+  }, [])
+
+  const updateEvent = (key, value) => {
+    setEvent((prevEvent) => ({ ...prevEvent, [key]: value }))
+  }
+
   const handleInputChange = (e) => {
     const { target } = e
     const { name } = target
     const value = target.type === "checkbox" ? target.checked : target.value
 
-    setEvent({ ...event, [name]: value })
+    updateEvent(name, value)
   }
 
   const renderErrors = () => {
@@ -95,7 +115,8 @@ const EventForm = () => {
             id="event_date"
             name="event_date"
             className="input-bordered input"
-            onChange={handleInputChange}
+            ref={dateInput}
+            autoComplete="off"
           />
         </div>
         <div className="form-control">
