1cc6e874f3416036f7d613fdb8da604571e1067d
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-01-13 20:08:46 +0900

    feat: 日付のバリデーションをより厳密に、ヘッダーをリンクに変更

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Header = () => (
   <header className="navbar bg-primary px-4 text-primary-content sm:px-6 lg:px-8">
-    <h1 className="text-3xl font-medium">Event Manager</h1>
+    <Link to="/events">
+      <h1 className="text-3xl font-medium">Event Manager</h1>
+    </Link>
   </header>
 )
 
