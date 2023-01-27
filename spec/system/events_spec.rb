b3ccbd28c0d0b1f7cbfcf9224d1ab2a0045e9eba
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-01-27 23:17:41 +0900

    test: イベントの詳細ページのテストを作成

--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -15,4 +15,49 @@ RSpec.describe "イベント", type: :system do
       expect(page).to have_current_path events_path
     end
   end
+
+  describe "詳細表示" do
+    let!(:event) { create(:event, event_type: "Colloquium", event_date: "2022-07-21".to_date, title: "Investigating the Battle of Hastings", speaker: "Sarah Croix", host: "Jim Bradbury", published: false) }
+
+    context "一覧ページから詳細ページに移動したとき" do
+      it "詳細が表示される" do
+        visit root_path
+
+        expect(page).to have_content "Colloquium"
+        expect(page).to have_current_path events_path
+
+        click_link "2022-07-21 - Colloquium"
+
+        expect(page).to have_content "Type: Colloquium"
+        expect(page).to have_content "Date: 2022-07-21"
+        expect(page).to have_content "Title: Investigating the Battle of Hastings"
+        expect(page).to have_content "Speaker: Sarah Croix"
+        expect(page).to have_content "Host: Jim Bradbury"
+        expect(page).to have_content "Published: no"
+        expect(page).to have_current_path "/events/#{event.id}"
+      end
+    end
+
+    context "詳細ページの URL に直接移動したとき" do
+      it "詳細が表示される" do
+        visit "/events/#{event.id}"
+
+        expect(page).to have_content "Type: Colloquium"
+        expect(page).to have_content "Date: 2022-07-21"
+        expect(page).to have_content "Title: Investigating the Battle of Hastings"
+        expect(page).to have_content "Speaker: Sarah Croix"
+        expect(page).to have_content "Host: Jim Bradbury"
+        expect(page).to have_content "Published: no"
+        expect(page).to have_current_path "/events/#{event.id}"
+      end
+    end
+
+    context "存在しない id を使った詳細ページの URL に直接移動したとき" do
+      it "EventNotFound コンポーネントが表示される" do
+        visit "/events/0"
+
+        expect(page).to have_content "Event not found!"
+      end
+    end
+  end
 end
