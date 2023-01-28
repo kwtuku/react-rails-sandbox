9e9580bd45fc6b1f341255e061464639dce96c72
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-01-28 23:09:26 +0900

    test: イベントの作成ページのテストを作成

--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -60,4 +60,77 @@ RSpec.describe "イベント", type: :system do
       end
     end
   end
+
+  describe "作成" do
+    context "入力内容が有効なとき" do
+      it "トーストメッセージが表示される、作成したイベントが表示される" do
+        visit root_path
+
+        expect(page).to have_link "New Event"
+        expect(page).to have_current_path events_path
+
+        click_link "New Event"
+
+        expect(page).to have_field "Type:"
+        expect(page).to have_current_path "/events/new"
+
+        fill_in "Type:", with: "Colloquium"
+        fill_in "Date:", with: "2022-04-12"
+        fill_in "Title:", with: "Scholasticism in Medieval and Early Modern History"
+        fill_in "Speakers:", with: "Robin Fleming"
+        fill_in "Hosts:", with: "Henry Louis Gates Jr."
+        check "Publish:"
+        click_button "Save"
+
+        expect(page).to have_content "Event Added!"
+        expect(page).to have_current_path(/\/events\/\d+/)
+
+        expect(page).to have_content "Type: Colloquium"
+        expect(page).to have_content "Date: 2022-04-12"
+        expect(page).to have_content "Title: Scholasticism in Medieval and Early Modern History"
+        expect(page).to have_content "Speaker: Robin Fleming"
+        expect(page).to have_content "Host: Henry Louis Gates Jr."
+        expect(page).to have_content "Published: yes"
+      end
+    end
+
+    context "入力内容が無効なとき" do
+      it "エラーメッセージが表示される" do
+        visit root_path
+
+        expect(page).to have_link "New Event"
+        expect(page).to have_current_path events_path
+
+        click_link "New Event"
+
+        expect(page).to have_field "Type:"
+        expect(page).to have_current_path "/events/new"
+
+        click_button "Save"
+
+        expect(page).to have_content "The following errors prohibited the event from being saved:"
+        expect(page).to have_current_path "/events/new"
+
+        expect(page).to have_content "You must enter an event type"
+        expect(page).to have_content "You must enter a valid date"
+        expect(page).to have_content "You must enter a title"
+        expect(page).to have_content "You must enter at least one speaker"
+        expect(page).to have_content "You must enter at least one host"
+      end
+    end
+
+    context "作成ページの URL に直接移動したとき" do
+      it "フォームが表示される" do
+        visit "/events/new"
+
+        expect(page).to have_field "Type:"
+        expect(page).to have_field "Date:"
+        expect(page).to have_field "Title:"
+        expect(page).to have_field "Speakers:"
+        expect(page).to have_field "Hosts:"
+        expect(page).to have_field "Publish:"
+        expect(page).to have_current_path "/events/new"
+      end
+    end
+  end
 end
